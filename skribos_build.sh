23ee8c68e54d196b6691751c01b9d7d243bf98e3
leonidlezner <kontakt@lezner.de>
2020-01-26 20:09:03 +0100

    Generating the version based on git tags

--- skribos_build.sh
+++ skribos_build.sh
@@ -6,32 +6,53 @@ language=$3
 defaults=${4-'lernos-de'}
 pdf_template='external/lernos-common/templates/eisvogel.tex'
 
+function calc_version() {
+    long_version=`git describe --tags --long | sed 's/-g[a-z0-9]\{7\}//'`
+    branch=`git rev-parse --abbrev-ref HEAD`
+
+    if [ ! $branch = 'master' ]; then
+        branch=" ($branch)"
+    else
+        branch=""
+    fi
+
+    export version="${long_version}${branch}"
+}
+
 function prepare_target {
     mkdir -p $output
 }
 
 function convert_pdf {
-    pandoc -o $output/$base.pdf -V lang=$language --template=$pdf_template --defaults $defaults
+    echo "Creating PDF format..."
+    pandoc -o $output/$base.pdf -V lang=$language -V date="$version" --template=$pdf_template --defaults $defaults
+    
+    echo "Creating cover image..."
     convert -density 300 $output/$base.pdf[0] $output/ebook-cover.png
 }
 
 function convert_epub {
-    pandoc -s -o $output/$base.epub --epub-cover-image=$output/ebook-cover.png --defaults $defaults
+    echo "Creating EPUB format..."
+    pandoc -s -o $output/$base.epub --epub-cover-image=$output/ebook-cover.png -V date="$version" --defaults $defaults
 }
 
 function convert_mobi {
+    echo "Creating MOBI format..."
     ebook-convert $output/$base.epub $output/$base.mobi
 }
 
 function convert_docx {
-    pandoc -o $output/$base.docx -V lang=$language --defaults $defaults
+    echo "Creating DOCX format..."
+    pandoc -o $output/$base.docx -V lang=$language -V date="$version" --defaults $defaults
 }
 
 function convert_html {
-    pandoc -o $output/$base.html -V lang=$language --extract-media ./images --defaults $defaults
+    echo "Creating HTML format..."
+    pandoc -o $output/$base.html -V lang=$language -V date="$version" --extract-media ./images --defaults $defaults
     cp -r ./images $output && rm -r ./images
 }
 
+calc_version
 prepare_target
 convert_html
 convert_pdf
